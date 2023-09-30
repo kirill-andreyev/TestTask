43227dd729bf23717f545a3ce0b2db18e51fd553
kirill-andreyev <kirilland02@gmail.com>
2023-09-30 16:28:46 +0300

    Added OrderService and UserService

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+        
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        async Task<Order> IOrderService.GetOrder()
+        {
+            var allOrders = await  _context.Orders.ToListAsync();
+            return allOrders.MaxBy(x => x.Price); //Returns the order with the highest amount. If more than one returns the first
+        }
+
+        async Task<List<Order>> IOrderService.GetOrders()
+        {
+            return await _context.Orders.Where(x => x.Quantity > 10).ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        async Task<User> IUserService.GetUser()
+        {
+            var allUsers = _context.Orders.GroupBy(x => x.UserId).Select(g => new
+            {
+                UserId = g.Key,
+                MaxNumberOfOrders = g.Count()
+            }).OrderByDescending(x => x.MaxNumberOfOrders).FirstOrDefault();
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == allUsers.UserId);
+            return user;
+        }
+
+        async Task<List<User>> IUserService.GetUsers()
+        {
+            return await _context.Users.Where(x => x.Status == Enums.UserStatus.Inactive).ToListAsync();
+        }
+    }
+}
